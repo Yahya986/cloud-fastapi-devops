9191eee2199841fc7fd76ac69469c565d315d52f
Mohammed Yahya Hussain <yahyahussain9704@gmail.com>
2026-05-15 22:07:15 +0530

    added health and system endpoints

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from datetime import datetime
+import platform
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "My first DevOps app is running!"}
+    return {
+        "project": "Cloud FastAPI DevOps Project",
+        "status": "running",
+        "message": "Yahya's AI-assisted DevOps app is live 🚀"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy",
+        "timestamp": str(datetime.now())
+    }
+
+@app.get("/system")
+def system():
+    return {
+        "python_version": platform.python_version(),
+        "system": platform.system(),
+        "machine": platform.machine()
+    }
